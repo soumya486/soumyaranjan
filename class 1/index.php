b551e7767940d503a94c22d5248c2241623cff04
soumya <luckyranjan486@gmail.com>
2019-11-13 09:36:02 +0530

    class1 index.php added

---
+++ class 1/index.php
@@ -0,0 +1,17 @@
+<?php
+$firstnumber=$_POST['FIRSTNUMBER'];
+$lastnumber=$_POST['LASTNUMBER'];
+echo ($firstnumber+$lastnumber);
+?>
+<html>
+<form method="post" action="">
+<input name="FIRSTNUMBER" type="number"></input>
+<input name="LASTNUMBER" type="number"></input> 
+<input type="submit"></input>
+</form>
+</html>   
+
+
+
+
+
